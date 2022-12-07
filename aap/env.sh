75f5d10848300d7da81943442bd561a04e249a93
Holden Fenner <hfenner@redhat.com>
2022-12-07 02:18:26 +0000

    Always auth 1Password and only run ssh-agent when necessary

--- aap/env.sh
+++ aap/env.sh
@@ -1,9 +1,10 @@
 #Sign in to One Password if necessary
-#eval $(op signin)
+eval $(op signin)
 export AWS_ACCESS_KEY_ID=$(op item get mq6tv6iavgezhk7foykfl6yvuy --fields username)
 export AWS_SECRET_ACCESS_KEY=$(op item get mq6tv6iavgezhk7foykfl6yvuy --fields credential)
 export AWS_REGION=us-east-2
 export NS=viper
 export LOGIC_REPO=${HOME}/rhdirt
-eval $(ssh-agent)
+#If you don't have an ssh-agent running
+#eval $(ssh-agent)
 aws ssm get-parameter --name "/${NS}/machine_ssh_private_key" --with-decryption --output text --query Parameter.Value | ssh-add -
